ef76bea749f222acb27e5fa66ca79bf418c5616a
xzorcious <simonrosengren123@gmail.com>
2017-05-13 21:07:30 +0200

    Putting javascript in seperate folder

---
+++ CommentProject/scripts/CommentProject/ApiScript.js
@@ -0,0 +1,22 @@
+﻿CommentProject = {
+    Api: {
+
+        /*ADD COMMENT*/
+        addComment: function addComment() {
+            $.post('api/Comment/Put', { "": $('#comment').val() });
+        },
+
+        /*GET ALL COMMENTS*/
+        getAllComments: function () {
+            // Send an AJAX request
+            $.getJSON('api/Comment')
+                .done(function (data) {
+                    // On success, 'data' contains a list of products.
+                    $.each(data, function (key, item) {
+                        // Add a list item for the product.
+                        $('<li>', { text: item.Message }).appendTo($('#comments'))
+                    })
+                })
+        }
+    }
+}
