c790e3c1de14573b135165487fc9848dd2ea2951
xzorcious <simonrosengren123@gmail.com>
2017-06-07 13:56:43 +0200

    adding mark up for comment

--- CommentProject/scripts/CommentProject/ApiScript.js
+++ CommentProject/scripts/CommentProject/ApiScript.js
@@ -1,15 +1,27 @@
 ﻿CommentProject = {
     Api: {
-        init: function () {
+        init: function (e) {
             $("#PostForm").submit(function () {
                 $.post('/api/Comment/Post', $("#PostForm").serialize())
             })
 
-            $("#SearchTopicForm").submit(function () {
+            $("#SearchTopicForm").submit(function (e) {
+                e.preventDefault()
                 $.get('/api/Comment/Search?' + $("#SearchTopicForm").serialize()).done(function (data) {
                     $.each(data, function (key, item) {
-                        // Add a list item for the product.
-                        $('<li>', { text: item.Message }).appendTo($('#comments'))
+                        const markup =
+                         `<div class="container">
+                            <div class="dialogbox">
+                                <div class="body">
+                                    <span class="author">${item.Author}</span>
+                                    <span class="tip tip-up"></span>
+                                    <div class="message">
+                                        <span>${item.Message}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`;
+                        document.getElementById("comments").innerHTML = markup;
                     })
                 })
             })
@@ -18,7 +30,7 @@
                 // On success, 'data' contains a list of products.
                 $.each(data, function (key, item) {
                     // Add a list item for the product.
-                   // $('<li>', { text: item.Message }).appendTo($('#comments'))
+                    // $('<li>', { text: item.Message }).appendTo($('#comments'))
                 })
             })
         }
