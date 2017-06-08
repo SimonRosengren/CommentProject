bdd1cef354c2c42710849ec542398003e9d2704d
xzorcious <simonrosengren123@gmail.com>
2017-06-08 10:22:21 +0200

    Adding graphical for comment on comment

--- CommentProject/scripts/CommentProject/ApiScript.js
+++ CommentProject/scripts/CommentProject/ApiScript.js
@@ -1,30 +1,30 @@
 ﻿CommentProject = {
     Api: {
+        /*Initialize all required functions*/
         init: function (e) {
             $("#PostForm").submit(function () {
                 $.post('/api/Comment/Post', $("#PostForm").serialize())
             })
-
             $("#SearchTopicForm").submit(function (e) {
                 e.preventDefault()
                 $.get('/api/Comment/Search?' + $("#SearchTopicForm").serialize()).done(function (data) {
                     const ul = document.getElementById("comments")
                     $.each(data, function (key, item) {
-                        const markup =
-                         `<div class="container">
-                            <div class="dialogbox">
-                                <div class="body">
-                                    <span class="author">${item.Author}</span>
-                                    <span class="tip tip-up"></span>
-                                    <div class="message">
-                                        <span>${item.Message}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>`;
+                        const markup = CommentProject.Api.CommentMarkUp(item)
                         var li = document.createElement("li")
                         li.innerHTML = markup
                         ul.appendChild(li)
+                        /*Create a new UL*/
+                        var childUl = document.createElement("ul")
+                        li.appendChild(childUl);
+                        /*Recursivley call for children*/
+                        $.get('/api/get/child?parent=' + item.ID).done(function (data) {
+                            $.each(data, function (key, item) {
+                                var childLi = document.createElement("li")
+                                childLi.innerHTML = CommentProject.Api.CommentMarkUp(item)
+                                childUl.appendChild(childLi)
+                            })
+                        })
                     })
                 })
             })
@@ -36,6 +36,31 @@
                     // $('<li>', { text: item.Message }).appendTo($('#comments'))
                 })
             })
+        },
+
+        /*Post a comment to a comment*/
+        Comment: function (data) {
+            $.post('/api/post/answer?answer=' + $("#commentInput").val() + "&parent=" + data)
+        },
+
+        /*Returns the Comment Mark up*/
+        CommentMarkUp: function (item) {
+            const markup =
+                `<div class="container">
+                    <div class="dialogbox">
+                        <div class="body">
+                            <span class="author">${item.Author}</span>
+                            <span class="tip tip-up"></span>
+                                <div class="message">
+                                    <span>${item.Message}</span>
+                                </div>
+                                <span class="comment" onclick="CommentProject.Api.Comment('${item.ID}')">Comment</span>
+                        </div>
+                        <form>
+                            <input id ="commentInput" name="answer" type="text" placeholder="comment..." />
+                        </form>
+                   </div>
+                </div>`            return markup;
         }
     }
 }
