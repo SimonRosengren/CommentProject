9ed314ea1a898fad54a026d4653a54eaa45a9ade
LuddeW <ludwig.wiktorsson@gmail.com>
2017-06-08 10:29:45 +0200

    Adding Links to new Views in the navbar

---
+++ CommentProject/Controllers/SearchTopicController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommentProject.Controllers
+{
+    public class SearchTopicController : Controller
+    {
+        // GET: SearchTopic
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
