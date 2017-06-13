043ea768f4213f62adee4673ffd08fd00b8b8ce0
LuddeW <ludwig.wiktorsson@gmail.com>
2017-06-13 09:05:12 +0200

    Removing unnecessary stuff

--- CommentProject/scripts/CommentProject/main.js
+++ CommentProject/scripts/CommentProject/main.js
@@ -1,15 +1,12 @@
 ﻿var CommentProject = {
     Init: function () {
-        CommentProject.Topic.Init()
-        this.HookEvents()
+        for (var prop in CommentProject) {
+            let child = CommentProject[prop]
+            if (typeof child ==='object' && child.hasOwnProperty('init')) {
+                child.Init()
+            }
+        }
     },
-
-    HookEvents: function () {
-        $("#postbutton").click(CommentProject.Topic.PostTopic)
-        $("#random").click(CommentProject.Topic.RandomTopic)
-        $("#SearchTopicForm").submit(CommentProject.Topic.Search)
-        $("#topics").on('click', '.topiclink', CommentProject.Topic.GetClickedTopic)
-    }
 }
 
 $(document).ready(function () {
